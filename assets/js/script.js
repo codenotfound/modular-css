021a1d3bb363a0b6210a2b4180bec66daeabe298
codenotfound <dmitry.velikij@gmail.com>
2016-01-14 17:26:41 +0300

    early bird

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    // var networks = ["dribbble", "vk", "instagram"];
+    // for(i=0; i<networks.length;i++){
+// ==================================================
+    // footer hover 
+// ==================================================
+    $(".footer__social-line--dribbble").hover(
+        function() {
+            $(this).children(".footer__social-img").attr("src","assets/img/dribbble_hover.png");
+            $(this).children(".footer__social-link").css("border-color", "#ea4c89"); 
+        },
+        function() {
+            $(this).children(".footer__social-img").attr("src","assets/img/dribbble.png");
+            $(this).children(".footer__social-link").css("border-color", "#D6D6D7"); 
+        }
+    );
+    $(".footer__social-line--vk").hover(
+        function() {
+            $(this).children(".footer__social-img").attr("src","assets/img/vk_hover.png");
+            $(this).children(".footer__social-link").css("border-color", "#1f4685"); 
+        },
+        function() {
+            $(this).children(".footer__social-img").attr("src","assets/img/vk.png");
+            $(this).children(".footer__social-link").css("border-color", "#D6D6D7"); 
+        }
+    );
+    $(".footer__social-line--instagram").hover(
+        function() {
+            $(this).children(".footer__social-img").attr("src",
+                "assets/img/instagram_hover.png");
+            $(this).children(".footer__social-link").css("border-color", "#a87e5e"); 
+        },
+        function() {
+            $(this).children(".footer__social-img").attr("src","assets/img/instagram.png");
+            $(this).children(".footer__social-link").css("border-color", "#D6D6D7"); 
+        }
+    );
+    
+});
